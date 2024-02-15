9622120418b68d12b0a306f0178beb522d507c80
ibobey <ibrahim-tunc-02@hotmail.com>
2024-02-15 13:43:21 +0300

    navbar created

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,6 +2,20 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <div className="navbar-left">
+        <div className="navbar-left-icons">
+          <i className="fa-solid fa-phone" id="navbar-left-icon-phone"></i>
+          <i className="fa-solid fa-envelope"></i>
+          <i className="fa-brands fa-whatsapp"></i>
+        </div>
+      </div>
+      <div className="navbar-center">
+        <i className="fa-solid fa-house"></i>
+      </div>
+      <div className="navbar-right">
+        <i className="fa-solid fa-user"></i>
+      </div>
+    </nav>
+  );
 }
