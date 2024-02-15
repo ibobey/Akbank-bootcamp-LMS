3d9b51c14a7d03b88f7b252a9b6ed991ae1770e5
ibobey <ibrahim-tunc-02@hotmail.com>
2024-02-15 15:35:35 +0300

    add book created

--- client/src/pages/add-book/AddBook.jsx
+++ client/src/pages/add-book/AddBook.jsx
@@ -1,7 +1,23 @@
 import "./AddBook.css";
-
+import Header from "../../components/header/Header";
 export default function AddBook() {
   return (
-    <div>AddBook</div>
-  )
+    <>
+      <Header />
+      <div className="page-add-book">
+        <div className="page-add-book-container">
+          <form>
+            <label>Add Book</label>
+            <input type="text" placeholder="book name" required />
+            <input type="text" placeholder="author" required />
+            <input type="date " placeholder="release date" />
+            <input type="text" placeholder="number of page" />
+
+            <button type="submit">Add</button>
+            <i className="fa-solid fa-trash"></i>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
