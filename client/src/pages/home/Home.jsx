cef8f18d951dcfc76d96fc986ebfa8ff6b064be7
ibobey <ibrahim-tunc-02@hotmail.com>
2024-02-15 15:02:31 +0300

    quick select inserted to home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,12 +5,24 @@ import QuickSelect from "./home-components/QuickSelect";
 export default function Home() {
   return (
     <>
-    <Header />
-      <QuickSelect
-        info="CONTACT"
-        source="https://cryptonairz.com/wp-content/uploads/2021/06/im-image-section-121.png"
-        routeTo="/add-book"
-      />
+      <Header />
+      <div className="page-home-container">
+        <QuickSelect
+          info="ADD BOOK"
+          source="https://cryptonairz.com/wp-content/uploads/2021/06/im-image-section-121.png"
+          routeTo="/add-book"
+        />
+        <QuickSelect
+          info="LIST BOOK"
+          source="https://cryptonairz.com/wp-content/uploads/2021/06/im-image-section-121.png"
+          routeTo="/list-book"
+        />
+        <QuickSelect
+          info="REMOVE BOOK"
+          source="https://cryptonairz.com/wp-content/uploads/2021/06/im-image-section-121.png"
+          routeTo="/remove-book"
+        />
+      </div>
     </>
   );
 }
